d2f0e043193bec1833d7ab53cd4d6d26cbcdff70
Wang Shuo <39981554+spacegt-master@users.noreply.github.com>
2025-03-20 13:51:15 +0800

    Delete web directory

--- web/app/components/tools/labels/constant.ts
+++
@@ -1,4 +0,0 @@
-export type Label = {
-  name: string
-  label: string
-}
--- web/app/components/workflow/note-node/note-editor/toolbar/divider.tsx
+++
@@ -1,7 +0,0 @@
-const Divider = () => {
-  return (
-    <div className='mx-1 w-[1px] h-3.5 bg-divider-regular'></div>
-  )
-}
-
-export default Divider
